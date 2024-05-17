94e5f39036f66ad9e017b866d44ebfd6071925b2
Daniel Augustynowicz <auhustsinovich.d@gmail.com>
2024-05-17 23:56:28 +0200

    Add code examples.

--- DemoApp/DemoApp/main.swift
+++ DemoApp/DemoApp/main.swift
@@ -1,1 +1,15 @@
+import collections
 
+var stack = Stack<Int>()
+var statsStack = StackStatistics<Int>()
+
+stack.push(1)
+stack.push(2)
+stack.push(3)
+
+statsStack.push(2)
+statsStack.push(0)
+statsStack.push(1)
+
+print("stack is empty - \(stack.isEmpty).")
+print("statsStack has - \(statsStack.count) elements.")
