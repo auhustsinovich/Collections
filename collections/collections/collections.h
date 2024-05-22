61826a05c3dcc36bcf2d3b7301229a6994753557
Daniel Augustynowicz <auhustsinovich.d@gmail.com>
2024-05-22 17:12:31 +0200

    Rename `collections` to `Collections` in project files.

--- collections/collections/collections.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-//! Project version number for collections.
-FOUNDATION_EXPORT double collectionsVersionNumber;
-
-//! Project version string for collections.
-FOUNDATION_EXPORT const unsigned char collectionsVersionString[];
